fa013ea9bf7f8d8b279c4493ec8a091072440123
Pupuu <137004973+Kim-Chaewonn@users.noreply.github.com>
2023-11-26 17:13:14 -0500

    Update Date.h

--- Date.h
+++ Date.h
@@ -14,13 +14,7 @@ I have done all the coding by myself and only copied the code
 that my professor provided to complete my workshops and assignments.
 -----------------------------------------------------------*/
 
-//==============================================
-// Name : Kabir Narula
-// Email : Knarula9@myseneca.ca
-// Student ID : 127962223
-// Section : NAA
-//==============================================
-// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 
 #ifndef SDDS_DATE_H__
 #define SDDS_DATE_H__
