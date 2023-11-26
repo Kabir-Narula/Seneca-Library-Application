6e7dccb33ae2c6f10f21e9bf6f7009b1157826fb
Pupuu <137004973+Kim-Chaewonn@users.noreply.github.com>
2023-11-26 17:14:33 -0500

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -14,13 +14,7 @@ I have done all the coding by myself and only copied the code
 that my professor provided to complete my workshops and assignments.
 -----------------------------------------------------------*/
 
-//==============================================
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-//==============================================
-//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 
 #ifndef SDDS_MENU_H_
 #define SDDS_MENU_H_
